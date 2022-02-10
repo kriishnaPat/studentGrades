8c3b740bb775c553a94bdc2b69cc49c9501ba668
Krishna Patel <kriishnapat@gmail.com>
2022-02-10 09:11:37 -0700

    sdfdf

--- app/Program.cs
+++ app/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 
-namespace app
+namespace StudentGrades
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] grades = {};
-
+            Console.Write("hello");
+            Random rnd = new Random();
+            for (int i = 0; i == 50; i++ ){
+                int[] grades = {rnd.Next(0,100)};
+                Console.WriteLine(grades);
+            }
+            
         }
     }
 }
